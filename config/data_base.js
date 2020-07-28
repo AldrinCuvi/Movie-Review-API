152d0524be31b34a74e8bd05ba64a79bed0d01c3
AldrinCuvi <aldrincuvi@hotmail.com>
2020-07-27 22:53:14 -0500

    a

--- config/data_base.js
+++ config/data_base.js
@@ -5,8 +5,14 @@
 
 const { Sequelize } = require('sequelize');
 
-//Datbase connection
+// //Datbase connection
+// module.exports = new Sequelize('BD_Movie_Review_App', 'postgres', 'Arj030509', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// });
+
+//Connection to Heroku
 module.exports = new Sequelize('daeoeit1q89cqs', 'sqhymiigtjcekr', '09a558686271a1bc857cda187efeba1346e1bbab6616c3c3176f987e35c23680', {
-    host: 'ec2-54-81-37-115.compute-1.amazonaws.com',
+    host: 'ec2-54-81-37-115.compute-1.amazonaws.com:5432',
     dialect: 'postgres'
 });
