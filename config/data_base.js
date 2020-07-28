7a3d3011d1f8a9659b3a8af1cc5b06bddfaad46c
AldrinCuvi <aldrincuvi@hotmail.com>
2020-07-27 22:40:59 -0500

    heroku correction

--- config/data_base.js
+++ config/data_base.js
@@ -7,6 +7,6 @@ const { Sequelize } = require('sequelize');
 
 //Datbase connection
 module.exports = new Sequelize('daeoeit1q89cqs', 'sqhymiigtjcekr', '09a558686271a1bc857cda187efeba1346e1bbab6616c3c3176f987e35c23680', {
-    host: 'ec2-54-81-37-115.compute-1.amazonaws.com:5432',
+    host: 'ec2-54-81-37-115.compute-1.amazonaws.com',
     dialect: 'postgres'
 });
