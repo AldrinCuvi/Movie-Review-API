3272d06d27fe1c48bd751926fb7aa66f18aecd17
AldrinCuvi <aldrincuvi@hotmail.com>
2020-07-28 14:20:21 -0500

    a

--- config/data_base.js
+++ config/data_base.js
@@ -12,7 +12,10 @@ const { Sequelize } = require('sequelize');
 // });
 
 //Connection to Heroku
-module.exports = new Sequelize('daeoeit1q89cqs', 'sqhymiigtjcekr', '09a558686271a1bc857cda187efeba1346e1bbab6616c3c3176f987e35c23680', {
+module.exports = new Sequelize({
+    username:'sqhymiigtjcekr',
+    password:'09a558686271a1bc857cda187efeba1346e1bbab6616c3c3176f987e35c23680',
+    database:'daeoeit1q89cqs',
     host: 'ec2-54-81-37-115.compute-1.amazonaws.com',
     dialect: 'postgres'
 });
